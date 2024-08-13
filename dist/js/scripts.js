38b5a08824c1ce3e7e9f00935170a213e8736508
Philip Procházka <ph.prochazka.git@gmail.com>
2024-08-13 10:40:16 +0200

    Initial edit

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - The Big Picture v5.0.6 (https://startbootstrap.com/template/the-big-picture)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
 */
 // This file is intentionally blank
